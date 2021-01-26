a747b208e5a0267fafcfe77cd7b1082126c26dae
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2021-01-26 18:11:44 +0100

    add palindrome checker solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 // complete the function
 function palindrom(str) {
   // code goes here
+  str = str.toLowerCase();
+  const spaceRegex = /\s/g;
+  const charRegex = /[-'/`!#*$~@_%+=.,^&(){}[\]|;:"<>?\\]/g;
+
+  // remove all white spaces before, after and in between the string
+  str = str.replace(spaceRegex, '');
+  // remove all special characters
+  str = str.replace(charRegex, '');
+
+  /* convert the string to an array,
+  reverse the array using the array reverse method,
+  convert the array back to a string.
+  */
+  const revStr = str.split('').reverse().join('');
+
+  if (str === revStr) {
+      return true;
+  } else {
+      return false;
+  }
 }
 
 function solution(arg) {
